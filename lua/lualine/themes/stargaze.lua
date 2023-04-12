62efbbc74cd9c069aaa15f2d84dcc4d1377b05a6
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2023-04-12 18:27:27 +0200

    feat: shuffle lualine.nvim colors around

--- lua/lualine/themes/stargaze.lua
+++ lua/lualine/themes/stargaze.lua
@@ -4,21 +4,21 @@ return {
 	normal = {
 		a = { fg = colors.fg, bg = colors.float },
 		b = { fg = colors.fg, bg = colors.bg2 },
-		c = { fg = colors.fg, bg = colors.bg },
+		c = { fg = colors.fg, bg = colors.bg2 },
 	},
 	insert = {
 		a = { fg = colors.bg, bg = colors.variable },
 		b = { fg = colors.bg, bg = colors.uri },
-		c = { fg = colors.fg, bg = colors.bg },
+		c = { fg = colors.fg, bg = colors.bg2 },
 	},
 	visual = {
 		a = { fg = colors.fg, bg = colors.trace },
 		b = { fg = colors.fg, bg = colors.trace1 },
-		c = { fg = colors.fg, bg = colors.bg },
+		c = { fg = colors.fg, bg = colors.bg2 },
 	},
 	replace = {
-		a = { fg = colors.fg, bg = colors.warn },
-		b = { fg = colors.fg, bg = colors.warn1 },
-		c = { fg = colors.fg, bg = colors.bg },
+		a = { fg = colors.fg, bg = colors.error },
+		b = { fg = colors.fg, bg = colors.error1 },
+		c = { fg = colors.fg, bg = colors.bg2 },
 	},
 }
